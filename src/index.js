eb0ae4a7f14c5cc2ad7b1c8b11dfc8e144555e7c
estacioj91 <estacioj91@yahoo.com>
2018-09-05 00:29:41 -0700

    js boilerplate
    js boilerplate using webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//import "./utilities"
+import {add} from "./utilities"
+import {scream} from "./scream"
+console.log("index.js");
+console.log(add(32,1));
+console.log(scream('possums'));
---
+++ src/utilities.js
@@ -0,0 +1,2 @@
+console.log("utilies.js");
+export const add =  (a,b) => a+b;
